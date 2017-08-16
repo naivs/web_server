e0df581447854efbe37a5fd4b9fde736f31cd1ac
nauiv <naumov5550123@gmail.com>
2017-08-16 21:23:28 +0300

    First commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,13 @@
+import org.eclipse.jetty.http.DateParser;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        long date = DateParser.parseDate("14-mar-1992 12:03:55");
+
+        for(int i = 0; i < 20; i++) {
+            System.out.println("Hello Mars!! Today: " + date);
+        }
+    }
+}
