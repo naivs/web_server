d0691dba2cbcbb5c493dcc6839712781e3d19497
nauiv <naumov5550123@gmail.com>
2017-08-16 21:35:17 +0300

    Message changed

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -7,7 +7,7 @@ public class Test {
         long date = DateParser.parseDate("14-mar-1992 12:03:55");
 
         for(int i = 0; i < 20; i++) {
-            System.out.println("Hello Mars!! Today: " + date);
+            System.out.println("Hello Red planet!! Today: " + date);
         }
     }
 }
