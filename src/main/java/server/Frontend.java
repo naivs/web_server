5c195cd133d8a688d490210366d8b24451dbf5fa
nauiv <naumov5550123@gmail.com>
2017-08-16 22:06:12 +0300

    Message changed

---
+++ src/main/java/server/Frontend.java
@@ -0,0 +1,22 @@
+package server;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Frontend extends HttpServlet {
+
+    private String login = "";
+
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
+    }
+
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
+    }
+}
