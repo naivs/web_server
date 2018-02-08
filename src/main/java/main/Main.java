1a1244d702b354afca5aee2079598313c791227b
Ivan <naumov5550123@gmail.com>
2018-02-08 12:52:29 +0300

    Task 8

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -5,20 +5,13 @@ import beans.AccountServiceController;
 import beans.AccountServiceControllerMBean;
 import beans.ResourceServerController;
 import beans.ResourceServerControllerMBean;
-import echoServer.Server;
 import services.dbService.DBServiceHibernate;
 import java.lang.management.ManagementFactory;
 import javax.management.MBeanServer;
 import javax.management.ObjectName;
-import resources.HBNParametersResource;
-import resources.HttpServerParametersResource;
-import resources.ResourceServer;
-import services.ServiceManager;
 import services.accountService.AccountService;
 import services.dbService.DBService;
 import services.server.HttpServer;
-import services.vfs.VFSImpl;
-import services.vfs.VFSService;
 import servlets.AdminServlet;
 import servlets.MirrorRequestServlet;
 import servlets.ResourceServlet;
@@ -31,38 +24,35 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        Thread server = new Thread(new Server(5050));
-        server.start();
-        
+//        Thread server = new Thread(new Server(5050));
+//        server.start();
         //ServiceManager serviceManager = new ServiceManager();
-
         //VFSService vfsService = new VFSImpl("./");
         //serviceManager.addService(VFSService.class.getName(), vfsService);
         //ResourceServer resourceServer = new ResourceServer(vfsService);
         //serviceManager.addService(ResourceServer.class.getName(), resourceServer);
+        DBService dbService = new DBServiceHibernate();
+        dbService.printConnectInfo();
+        //serviceManager.addService(DBService.class.getName(), dbService);
+        AccountService accountService = new AccountServiceImpl(10, dbService);
+        //serviceManager.addService(AccountService.class.getName(), accountService);
         
-//        DBService dbService = new DBServiceHibernate();
-//        dbService.printConnectInfo();
-//        //serviceManager.addService(DBService.class.getName(), dbService);
-//        AccountService accountService = new AccountServiceImpl(10, dbService);
-//        //serviceManager.addService(AccountService.class.getName(), accountService);
-//        
-//        MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
-//        AccountServiceControllerMBean accountController = new AccountServiceController(accountService);
-//        mbs.registerMBean(accountController, new ObjectName("SessionsManager:type=AccountServiceController"));
-//        ResourceServerControllerMBean resourceController = new ResourceServerController();
-//        mbs.registerMBean(resourceController, new ObjectName("Admin:type=ResourceServerController"));
-//
-//        HttpServer httpServer = new HttpServer();
-//        httpServer.addServlet(new RootRequestsServlet(accountService), "/");
-//        httpServer.addServlet(new SignupServlet(accountService), "/signup");
-//        httpServer.addServlet(new SigninServlet(accountService), "/signin");
-//        httpServer.addServlet(new MirrorRequestServlet(), "/mirror");
-//        httpServer.addServlet(new WebSocketChatServlet(), "/chat");
-//        httpServer.addServlet(new AdminServlet(accountService), AdminServlet.SERVLET_URL);
-//        httpServer.addServlet(new ResourceServlet(), ResourceServlet.SERVLET_URL);
-//        
-//        httpServer.start();
-//        httpServer.join();
+        MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
+        AccountServiceControllerMBean accountController = new AccountServiceController(accountService);
+        mbs.registerMBean(accountController, new ObjectName("SessionsManager:type=AccountServiceController"));
+        ResourceServerControllerMBean resourceController = new ResourceServerController();
+        mbs.registerMBean(resourceController, new ObjectName("Admin:type=ResourceServerController"));
+
+        HttpServer httpServer = new HttpServer();
+        httpServer.addServlet(new RootRequestsServlet(accountService), "/");
+        httpServer.addServlet(new SignupServlet(accountService), "/signup");
+        httpServer.addServlet(new SigninServlet(accountService), "/signin");
+        httpServer.addServlet(new MirrorRequestServlet(), "/mirror");
+        httpServer.addServlet(new WebSocketChatServlet(), "/chat");
+        httpServer.addServlet(new AdminServlet(accountService), AdminServlet.SERVLET_URL);
+        httpServer.addServlet(new ResourceServlet(), ResourceServlet.SERVLET_URL);
+        
+        httpServer.start();
+        httpServer.join();
     }
 }
